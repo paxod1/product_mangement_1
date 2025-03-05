84dd10a0ef7b06fc94f6e8918c4e2721d43a82c1
paxod1 <156282097+paxod1@users.noreply.github.com>
2025-03-05 13:38:58 +0530

    Update AddProduct.jsx

--- Frontend/src/pages/AddProduct.jsx
+++ Frontend/src/pages/AddProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { UserTokenRequest } from '../AxiosCreate';
-import './Adproduct.scss';
+import './AddProduct.scss';
 import { useNavigate } from 'react-router-dom';
 import UserNavbar from './UserNavbar';
 import { useSelector } from 'react-redux';
